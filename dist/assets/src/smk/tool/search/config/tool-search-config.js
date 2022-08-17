1bb1e46bbf49abfe52f199607486b09c01bb31ee
Darren Boss <Darren.Boss@gov.bc.ca>
2022-08-17 16:56:55 -0700

    Temporarily adding a dist directory containing custom SMK code for demo purposes.

---
+++ dist/assets/src/smk/tool/search/config/tool-search-config.js
@@ -0,0 +1,29 @@
+include.module( 'tool-search-config', [
+    'tool-config.tool-base-config-js',
+    'tool-config.tool-widget-config-js',
+    'tool-config.tool-panel-config-js',
+    'tool-config.tool-panel-feature-config-js',
+], function ( inc ) {
+    "use strict";
+
+    SMK.CONFIG.tools.push(
+        inc[ 'tool-config.tool-base-config-js' ](
+        inc[ 'tool-config.tool-widget-config-js' ](
+        inc[ 'tool-config.tool-panel-config-js' ](
+        inc[ 'tool-config.tool-panel-feature-config-js']( {
+            type: 'search',
+            enabled: true,
+            order: 2,
+            position: 'toolbar',
+            icon: 'search',
+            title: 'Search for Location',
+            showPanel: true,
+            showLocation: true,
+            command: {
+                identify: true,
+                measure: true,
+                directions: true,
+            }
+        } ) ) ) )
+    )
+} )
